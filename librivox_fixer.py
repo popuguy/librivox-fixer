4466c9c12764b03e7b1c554884b201fde9a3c821
Paul Cantrell <paulcantr@gmail.com>
2014-04-21 21:44:36 -0700

    Changed to functions

--- librivox_fixer.py
+++ librivox_fixer.py
@@ -1,11 +1,16 @@
 from pydub import AudioSegment
 
+intro_length = 18 * 1000 #millis
+outro_length = 12 * 1000 #millis
+
+def remove_intro_outro(audio, len_intro, len_outro):
+	return clip(audio, len_intro, len(audio) - len_outro)
+def clip(audio, begin, end):
+	return audiobook[begin:end]
 if __name__ == "__main__":
 	if len(argv) < 2:
 		print "Usage: %s filename.mp3" % argv[0]
 		exit(0)
 	audiobook = AudioSegment.from_mp3(argv[1])
-	intro_length = 18 * 1000 #ms
-	outro_length = 12 * 1000 #ms
-	audiobook = audiobook[intro_length:len(audiobook) - outro_length]
+	audiobook = remove_intro_outro(audiobook, intro_length, outro_length)
 	audiobook.export("%s_fix.mp3" % argv[1].split(".")[0], format="mp3")
